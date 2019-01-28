10f3b474b825c476b33a19bd35d3be414db6f8a0
Brian McMahon <30627595+cipher813@users.noreply.github.com>
2019-01-27 16:09:16 -0800

    fully trained model

--- scripts/statistics.py
+++ scripts/statistics.py
@@ -27,7 +27,7 @@ class Stats():
         self.best_avg_score = -np.Inf            # best score for a single episode
         self.time_start = time.time()            # track cumulative wall time
         self.total_steps = 0                     # track cumulative steps taken
-        self.writer = SummaryWriter()
+        self.writer = SummaryWriter("../results/")
 
     def update(self, steps, rewards, i_episode):
         """Update stats after each episode."""
@@ -73,9 +73,9 @@ class Stats():
     def print_epoch(self, i_episode, stats_format, *args):
         n_secs = int(time.time() - self.time_start)
         # common_stats = 'Episode: {:5}   Avg: {:8.3f}   BestAvg: {:8.3f}   σ: {:8.3f}  |  Steps: {:8}   Secs: {:6}      |  '.format(i_episode, self.avg_score, self.best_avg_score, self.std_dev, self.total_steps, n_secs)
-        common_stats = f'Episode:{i_episode:5}\tAvg:{self.avg_score:8.3f}\tBestAvg:{self.best_avg_score:8.3f}\tσ:{self.std_dev:8.3f}\tSteps:{steps:8}\tSecs:{n_secs:6}'
+        common_stats = f'Episode:{i_episode:5}\tAvg:{self.avg_score:8.3f}\tBestAvg:{self.best_avg_score:8.3f}\tσ:{self.std_dev:8.3f}\tSteps:{self.total_steps:8}\tSecs:{n_secs:6}'
         print('\r' + common_stats + stats_format.format(*args))
 
     def print_solve(self, i_episode, stats_format, *args):
         self.print_epoch(i_episode, stats_format, *args)
-        print('\nSolved in {:d} episodes!'.format(i_episode-100))
+        print(f'\nSolved in {i_episode:d} episodes!')
